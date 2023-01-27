43c602453bc9b5ddc869737feaa99b5d03c8c946
tkedjp <120151701+tkedjp@users.noreply.github.com>
2023-01-27 16:27:59 +0900

    スクレイピング

---
+++ jalan_csv.py
@@ -0,0 +1,123 @@
+from time import sleep
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+hotel_list = []
+
+# year = input('チェックインする年を入力してください：')
+month = input('チェックインする月を入力してください：')
+day = input('チェックインする日を入力してください：')
+stay_count = input('泊数を入力してください：')
+room_count = input('室数を入力してください：')
+adult_num = input('人数を入力してください：')
+
+base_url = 'https://www.jalan.net/040000/LRG_040200/SML_040202/?screenId=UWW1402&distCd=01&listId=0&activeSort=0&mvTabFlg=1&stayYear=2023&stayMonth=' + month + '&stayDay=' + day + '&stayCount=' + stay_count + '&roomCount=' + room_count +'&adultNum=' + adult_num +'&yadHb=1&roomCrack=200000&kenCd=040000&lrgCd=040200&smlCd=040202&vosFlg=6&idx={}'
+
+sleep(3)
+
+r = requests.get(base_url, timeout=7.5)
+if r.status_code >= 400:
+    print(F'{base_url}は無効です')
+
+soup = BeautifulSoup(r.content, 'lxml')
+
+number = soup.select_one('td.jlnpc-planListCnt-header > span.s16_F60b').text
+max_page_index = int(number) // 30 + 1
+# print(max_page_index)
+
+for i in range(max_page_index):
+    url = base_url.format(30*i)
+
+    sleep(3)
+
+    page_r = requests.get(url, timeout=7.5)
+    if page_r.status_code >= 400:
+        print(F'{url}は無効です')
+        continue
+
+    page_soup = BeautifulSoup(page_r.content, 'lxml')
+
+    #最安料金と1人あたりの料金
+    table_soup = page_soup.select('div.p-yadoCassette__body.p-searchResultItem__body')
+    for table in table_soup:
+        hotel = table.select_one('div.p-yadoCassette__body.p-searchResultItem__body > a > div > div > div.p-searchResultItem__summaryInner > div.p-searchResultItem__summaryLeft > h2').text
+        room_price = table.select_one('div.p-yadoCassette__body.p-searchResultItem__body > a > div > div > div.p-searchResultItem__summaryInner > div.p-searchResultItem__summaryRight > dl > dd > span.p-searchResultItem__lowestPriceValue').text
+        per_price = table.select_one('div.p-yadoCassette__body.p-searchResultItem__body > a > div > div > div.p-searchResultItem__summaryInner > div.p-searchResultItem__summaryRight > dl > dd > span.p-searchResultItem__lowestUnitPrice').text
+        page_urls = table.select('div.p-yadoCassette__body.p-searchResultItem__body > a.jlnpc-yadoCassette__link')
+        
+        sleep(3)
+
+        for i, page_url in enumerate(page_urls):
+            page_url = 'https://www.jalan.net' + page_url.get('href')
+
+            sleep(3)
+
+            hotel_page_r = requests.get(page_url, timeout=7.5)
+            if hotel_page_r.status_code >= 400:
+                print(F'{page_url}は無効です')
+                continue
+        
+            #ホテル名
+            hotel_page_soup = BeautifulSoup(hotel_page_r.content, 'lxml')
+            # hotel_name = hotel_page_soup.select_one('#pankuzu > h1').text
+
+            #住所
+            address_tags = hotel_page_soup.select_one('#jlnpc-main-contets-area > div.shisetsu-accesspartking_body_wrap > table tr:nth-child(1) > td')
+            address = address_tags.text
+            address = address.replace('大きな地図をみる', '')
+            address = address.strip()
+        
+            #駐車場
+            parking_tags = hotel_page_soup.select_one('#jlnpc-main-contets-area > div.shisetsu-accesspartking_body_wrap > table tr:nth-child(3) > td').text        
+            parking = parking_tags.replace('\n','')
+            parking = parking.strip()
+
+            #タイプ別の室数
+            room_tags = hotel_page_soup.select_one('.shisetsu-roomsetsubi_body')
+            tags = room_tags.text
+
+            if '総部屋数' not in tags:
+                single = room_tags.select_one('.shisetsu-roomsetsubi_body > tr:nth-child(2) > td > div > table tr:nth-child(2) > td:first-child').text
+                double = room_tags.select_one('.shisetsu-roomsetsubi_body > tr:nth-child(2) > td > div > table tr:nth-child(2) > td:nth-child(2)').text
+                twin = room_tags.select_one('.shisetsu-roomsetsubi_body > tr:nth-child(2) > td > div > table tr:nth-child(2) > td:nth-child(3)').text
+                sweet = room_tags.select_one('.shisetsu-roomsetsubi_body > tr:nth-child(2) > td > div > table tr:nth-child(2) > td:last-child').text
+                total = None
+
+            elif 'シングル' not in tags:
+                single = None
+                double = None
+                twin = None
+                sweet = None
+                total = room_tags.select_one('.shisetsu-roomsetsubi_body > tr:nth-child(1) > td > div > table tr:nth-child(2) > td:nth-child(5)').text
+                total = total.strip()
+
+            else:
+                single = room_tags.select_one('.shisetsu-roomsetsubi_body > tr:nth-child(3) > td > div > table tr:nth-child(2) > td:first-child').text
+                double = room_tags.select_one('.shisetsu-roomsetsubi_body > tr:nth-child(3) > td > div > table tr:nth-child(2) > td:nth-child(2)').text
+                twin = room_tags.select_one('.shisetsu-roomsetsubi_body > tr:nth-child(3) > td > div > table tr:nth-child(2) > td:nth-child(3)').text
+                sweet = room_tags.select_one('.shisetsu-roomsetsubi_body > tr:nth-child(3) > td > div > table tr:nth-child(2) > td:last-child').text
+                total = room_tags.select_one('.shisetsu-roomsetsubi_body > tr:nth-child(1) > td > div > table tr:nth-child(2) > td:nth-child(5)').text
+                total = total.strip()
+
+            sleep(3)
+
+            hotel_list.append({
+                'ホテル名': hotel,
+                '詳細ページ': page_url,
+                '住所': address,
+                'シングル': single,
+                'ダブル': double,
+                'ツイン': twin,
+                'スイート': sweet,
+                '総部屋数': total,
+                '室料': room_price,
+                '1人あたり': per_price,
+                '駐車場': parking
+            })
+            print(hotel_list[-1])
+
+#csv出力
+df = pd.DataFrame(hotel_list)
+df.to_csv('list.csv', index=False, encoding='utf-8-sig')
