faa1ebaeae8cc610e5a796d7a291b912de5fe961
tkedjp <120151701+tkedjp@users.noreply.github.com>
2023-02-02 17:56:59 +0900

    スプレッドシートに保存してwordに書込

--- jalan_sps.py
+++ jalan_sps.py
@@ -135,12 +135,67 @@ from gspread_dataframe import set_with_dataframe
 
 #認証
 SCOPES = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-SERVICE_ACCOUNT_FILE = 'virtual-firefly-363001-cb50276b19b7.json'
+SERVICE_ACCOUNT_FILE = 'xxxxxx.json'
 credentials = ServiceAccountCredentials.from_json_keyfile_name(SERVICE_ACCOUNT_FILE, SCOPES)
 gs = gspread.authorize(credentials)
-SPREADSHEET_KEY = '18eKi7qv6vwsT_E2OQbSrIJdn_qyISrJyAlNOiu1QctA'
+SPREADSHEET_KEY = 'xxxxxxx'
 
 #書込
 df = pd.DataFrame(hotel_list)
 workbook = gs.open_by_key(SPREADSHEET_KEY)
 set_with_dataframe(workbook.worksheet("シート1"), df, include_index=False)
+
+#docx
+import docx
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+
+# wordファイルを新規作成
+doc = docx.Document()
+
+#読込
+worksheet= gs.open_by_key(SPREADSHEET_KEY).sheet1
+#カラムを1行目に指定し，2行目以降の値を取得してデータフレームを作成
+df = pd.DataFrame(worksheet.get_all_values()[1:],columns=worksheet.get_all_values()[0])
+# print(df)
+
+for i, r in df.iterrows():
+    # print(r['ホテル名'], r['詳細ページ'])
+    if i == '':
+        break
+
+    #表題
+    doc.add_heading(r['ホテル名'], 0)
+    doc.add_paragraph('住所：'+ r['住所'])
+
+    #部屋タイプ内訳をテーブルにする
+    table = doc.add_table(rows=1, cols=5)
+
+    header_cells = table.rows[0].cells
+    header_cells[0].text = 'シングル'
+    header_cells[1].text = 'ダブル'
+    header_cells[2].text = 'ツイン'
+    header_cells[3].text = 'スイート'
+    header_cells[4].text = '総部屋数'
+
+    row_cells = table.add_row().cells
+    row_cells[0].text = (str(r['シングル']))
+    row_cells[1].text = (str(r['ダブル']))
+    row_cells[2].text = (str(r['ツイン']))
+    row_cells[3].text = (str(r['スイート']))
+    row_cells[4].text = (str(r['総部屋数']))
+
+    doc.add_paragraph('')
+    doc.add_paragraph('室料：'+ str(r['室料']))
+    doc.add_paragraph('1人あたり：'+ str(r['1人あたり']))
+    doc.add_paragraph('駐車場：'+ r['駐車場'].strip())
+    doc.add_paragraph('詳細ページ：'+ r['詳細ページ'])
+
+    # 改ページ
+    doc.add_page_break()
+
+# すべての行を左揃えにする
+for p in doc.paragraphs:
+    p.alignment = WD_ALIGN_PARAGRAPH.LEFT
+    
+# ファイルを保存
+doc.save('hotel_list.docx')
