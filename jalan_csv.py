cf6c648b9609e5dbe9071b805098a5e33da473cc
tkedjp <120151701+tkedjp@users.noreply.github.com>
2023-02-04 21:39:07 +0900

    ページ数の計算式修正など

--- jalan_csv.py
+++ jalan_csv.py
@@ -4,6 +4,8 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+import math
+
 hotel_list = []
 
 # year = input('チェックインする年を入力してください：')
@@ -17,24 +19,32 @@ adult_num = input('人数を入力してください：')
 
 base_url = 'https://www.jalan.net/040000/LRG_040200/SML_040202/?screenId=UWW1402&distCd=01&listId=0&activeSort=0&mvTabFlg=1&stayYear=2023&stayMonth=' + month + '&stayDay=' + day + '&stayCount=' + stay_count + '&roomCount=' + room_count +'&adultNum=' + adult_num +'&yadHb=1&roomCrack=200000&kenCd=040000&lrgCd=040200&smlCd=040202&vosFlg=6&idx={}'
 
+user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
+header = {
+    'User-Agent': user_agent
+}
+
 sleep(3)
 
-r = requests.get(base_url, timeout=7.5)
+r = requests.get(base_url, timeout=7.5, headers=header)
 if r.status_code >= 400:
     print(F'{base_url}は無効です')
 
 soup = BeautifulSoup(r.content, 'lxml')
 
+#詳細ページ数と一覧ページ数取得
 number = soup.select_one('td.jlnpc-planListCnt-header > span.s16_F60b').text
-max_page_index = int(number) // 30 + 1
+max_page_index = int(number) // 59.5 + 1
+max_page_index = math.floor(max_page_index)
 # print(max_page_index)
 
 for i in range(max_page_index):
     url = base_url.format(30*i)
+    # print(f'{i+1}ページ目URL：{url}')
 
     sleep(3)
 
-    page_r = requests.get(url, timeout=7.5)
+    page_r = requests.get(url, timeout=7.5, headers=header)
     if page_r.status_code >= 400:
         print(F'{url}は無効です')
         continue
@@ -48,16 +58,13 @@ for i in range(max_page_index):
         room_price = table.select_one('a > div > div > div.p-searchResultItem__summaryInner > div.p-searchResultItem__summaryRight > dl > dd > span.p-searchResultItem__lowestPriceValue').text
         per_price = table.select_one('a > div > div > div.p-searchResultItem__summaryInner > div.p-searchResultItem__summaryRight > dl > dd > span.p-searchResultItem__lowestUnitPrice').text
         page_urls = table.select('a.jlnpc-yadoCassette__link')
-        # print(hotel,room_price,per_price,page_urls)
-        
-        # sleep(3)
 
         for i, page_url in enumerate(page_urls):
             page_url = 'https://www.jalan.net' + page_url.get('href')
 
             sleep(3)
 
-            hotel_page_r = requests.get(page_url, timeout=7.5)
+            hotel_page_r = requests.get(page_url, timeout=7.5, headers=header)
             if hotel_page_r.status_code >= 400:
                 print(F'{page_url}は無効です')
                 continue
@@ -88,32 +95,38 @@ for i in range(max_page_index):
 
             #タイプ別の室数
             room_tag = hotel_page_soup.select_one('.shisetsu-roomsetsubi_body')
-            tags = room_tag.text
-
-            if '総部屋数' not in tags:
-                single = room_tag.select_one('tr:nth-child(2) > td > div > table tr:nth-child(2) > td:first-child').text
-                double = room_tag.select_one('tr:nth-child(2) > td > div > table tr:nth-child(2) > td:nth-child(2)').text
-                twin = room_tag.select_one('tr:nth-child(2) > td > div > table tr:nth-child(2) > td:nth-child(3)').text
-                sweet = room_tag.select_one('tr:nth-child(2) > td > div > table tr:nth-child(2) > td:last-child').text
-                total = None
-
-            elif 'シングル' not in tags:
+            if room_tag is None:
                 single = None
                 double = None
                 twin = None
                 sweet = None
-                total = room_tag.select_one('tr:nth-child(1) > td > div > table tr:nth-child(2) > td:nth-child(5)').text
-                total = total.strip()
+                total = None
 
             else:
-                single = room_tag.select_one('tr:nth-child(3) > td > div > table tr:nth-child(2) > td:first-child').text
-                double = room_tag.select_one('tr:nth-child(3) > td > div > table tr:nth-child(2) > td:nth-child(2)').text
-                twin = room_tag.select_one('tr:nth-child(3) > td > div > table tr:nth-child(2) > td:nth-child(3)').text
-                sweet = room_tag.select_one('tr:nth-child(3) > td > div > table tr:nth-child(2) > td:last-child').text
-                total = room_tag.select_one('tr:nth-child(1) > td > div > table tr:nth-child(2) > td:nth-child(5)').text
-                total = total.strip()
+                tags = room_tag.text
 
-            # sleep(3)
+                if '総部屋数' not in tags:
+                    single = room_tag.select_one('tr:nth-child(2) > td > div > table tr:nth-child(2) > td:first-child').text
+                    double = room_tag.select_one('tr:nth-child(2) > td > div > table tr:nth-child(2) > td:nth-child(2)').text
+                    twin = room_tag.select_one('tr:nth-child(2) > td > div > table tr:nth-child(2) > td:nth-child(3)').text
+                    sweet = room_tag.select_one('tr:nth-child(2) > td > div > table tr:nth-child(2) > td:last-child').text
+                    total = None
+
+                elif 'シングル' not in tags:
+                    single = None
+                    double = None
+                    twin = None
+                    sweet = None
+                    total = room_tag.select_one('tr:nth-child(1) > td > div > table tr:nth-child(2) > td:nth-child(5)').text
+                    total = total.strip()
+
+                else:
+                    single = room_tag.select_one('tr:nth-child(3) > td > div > table tr:nth-child(2) > td:first-child').text
+                    double = room_tag.select_one('tr:nth-child(3) > td > div > table tr:nth-child(2) > td:nth-child(2)').text
+                    twin = room_tag.select_one('tr:nth-child(3) > td > div > table tr:nth-child(2) > td:nth-child(3)').text
+                    sweet = room_tag.select_one('tr:nth-child(3) > td > div > table tr:nth-child(2) > td:last-child').text
+                    total = room_tag.select_one('tr:nth-child(1) > td > div > table tr:nth-child(2) > td:nth-child(5)').text
+                    total = total.strip()
 
             hotel_list.append({
                 'ホテル名': hotel,
