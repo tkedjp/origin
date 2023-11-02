bda69721c17158c26d721ecb4c81e93406cfb9c9
tkedjp <120151701+tkedjp@users.noreply.github.com>
2023-11-03 07:45:00 +0900

    Add files via upload

---
+++ athome_c.py
@@ -0,0 +1,45 @@
+import os
+from time import sleep
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+
+options = webdriver.ChromeOptions()
+# options.add_argument('--incognito')
+# options.add_argument('--headless')
+
+driver = webdriver.Chrome(
+    executable_path='/Users/takashie/Desktop/Lesson/hayatasu/section08/tools/chromedriver',
+    options=options)
+driver.implicitly_wait(10)
+
+# max_page_index = 188
+
+base_url = 'https://www.athome.co.jp/estate/osaka/list/?pref=27'
+
+url = driver.get(base_url)
+sleep(3)
+
+page_links = driver.find_elements_by_css_selector(
+    '.card-box__area')
+print(page_links)
+links = [page_link.get_attribute('href') for page_link in page_links]
+print(links)
+
+#pythonファイルのあるディレクトリパス取得
+dir_path = os.path.dirname(os.path.abspath(__file__))
+
+for i, link in enumerate(links):
+    print('='*30, i, '='*30)
+    print(link)
+    driver.get(link)
+    sleep(5)
+
+    html = driver.page_source
+
+    #dir_pathの中のhtmlというフォルダに，ファイル名{driver.title}.htmlで保存できる
+    p = os.path.join(dir_path, 'html', f'{driver.title}.html')
+    with open(p, 'w') as f:
+        f.write(html)
+
+sleep(3)
+driver.quit()
