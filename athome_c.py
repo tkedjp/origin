bb48d5611822ba39033e37d9fd62ee08161d1177
tkedjp <120151701+tkedjp@users.noreply.github.com>
2023-11-03 10:54:20 +0900

    Add files via upload

--- athome_c.py
+++ athome_c.py
@@ -12,29 +12,33 @@ driver = webdriver.Chrome(
     options=options)
 driver.implicitly_wait(10)
 
+#数字書き換える
 max_page_index = 188
 
+#地域書き換える
 base_url = 'https://www.athome.co.jp/estate/osaka/list/page{}/'
 
 for i in range(max_page_index):
-    base_url = base_url.format(1+i)
+    page_url = base_url.format(1+i)
+    print(f'{i+1}ページ目URL：{page_url}')
+    # print(page_url)
 
     sleep(3)
 
-    driver.get(base_url)
+    driver.get(page_url)
     sleep(3)
 
     page_links = driver.find_elements_by_css_selector(
         '.card-box__area a:first-of-type')
     links = [page_link.get_attribute('href') for page_link in page_links]
-    print(links)
+    # print(links)
 
     #pythonファイルのあるディレクトリパス取得
     dir_path = os.path.dirname(os.path.abspath(__file__))
-    print(dir_path)
+    # print(dir_path)
 
     for i, link in enumerate(links):
-        print('='*30, i, '='*30)
+        print('='*30, 1+i, '='*30)
         print(link)
         driver.get(link)
         sleep(5)
